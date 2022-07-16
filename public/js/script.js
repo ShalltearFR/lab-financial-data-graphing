891df5f81534f3d59485e4dd311dba176b5ceb0f
unknown <Jossuha@hotmail.fr>
2022-07-16 11:02:06 +0200

    Iteration 1 OK

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,12 @@ document.addEventListener(
   },
   false
 );
+
+axios({
+  method: "GET",
+  url: `http://api.coindesk.com/v1/bpi/historical/close.json`
+})
+  .then(function (response) {
+    console.log("ok", response.data);
+  })
+  .catch(err => console.log(err))
