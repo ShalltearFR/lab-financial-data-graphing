aa81e0272434f7fc91d6ebf9c02db6168743f141
lescata <alexandre.lescat@gmail.com>
2022-07-16 12:07:17 +0200

    iteration2

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,8 +1,31 @@
+let arr = [];
+const ctx = document.getElementById("canvas").getContext("2d");
+
 axios({
-    method: "GET",
-    url: `http://api.coindesk.com/v1/bpi/historical/close.json`
+  method: "GET",
+  url: `http://api.coindesk.com/v1/bpi/historical/close.json`,
+})
+  .then(function (response) {
+    console.log("ok", response.data);
+    Object.keys(response.data.bpi).forEach((key) => {
+      const value = response.data.bpi[key];
+      arr.push(value);
+      console.log("arr = ", arr);
+    });
+    const chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: Object.keys(response.data.bpi),
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: arr,
+            fill: true,
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+          },
+        ],
+      },
+    });
   })
-    .then(function (response) {
-      console.log("ok", response.data);
-    })
-    .catch(err => console.log(err))
+  .catch((err) => console.log(err));
