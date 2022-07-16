1926f9db56661b34cbdb58e3bf8ee6ce0e27aa96
unknown <Jossuha@hotmail.fr>
2022-07-16 14:22:13 +0200

    Iteration 3 OK

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,31 +1,80 @@
 let arr = [];
 const ctx = document.getElementById("canvas").getContext("2d");
+let fromDate = getActualDate(true)
+inputFromDate.value = fromDate
 
-axios({
-  method: "GET",
-  url: `http://api.coindesk.com/v1/bpi/historical/close.json`,
-})
-  .then(function (response) {
-    console.log("ok", response.data);
-    Object.keys(response.data.bpi).forEach((key) => {
-      const value = response.data.bpi[key];
-      arr.push(value);
-      console.log("arr = ", arr);
-    });
-    const chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: Object.keys(response.data.bpi),
-        datasets: [
-          {
-            label: "My First Dataset",
-            data: arr,
-            fill: true,
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.1,
-          },
-        ],
-      },
-    });
+let toDate = getActualDate(false)
+inputToDate.value = toDate
+
+let chart = null
+let currency = "EUR"
+
+callGraph()
+
+// Format la date
+function getActualDate(isFrom){
+    day = new Date().getDate()
+    if (isFrom){
+        month = new Date().getMonth() // Mois actuel - 1
+    }else {
+        month = new Date().getMonth() + 1 // Mois actuel
+    }
+    
+     // Rajoute un 0 avant le mois s'il en en dessous de 10 pour formater la date
+    if (month.toString().length === 1){ month = `0${month}`}
+    year = new Date().getFullYear()
+
+    return `${year}-${month}-${day}`
+}
+
+ // Creer et affiche le graphique
+function callGraph(){
+    axios({
+        method: "GET",
+        url: `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}`,
+      })
+        .then(function (response) {
+          console.log("ok", response.data);
+          Object.keys(response.data.bpi).forEach((key) => {
+            const value = response.data.bpi[key];
+            arr.push(value);
+            //console.log("arr = ", arr);
+          });
+
+          if (chart !== null){ chart.destroy()}
+          chart = new Chart(ctx, {
+            type: "line",
+            data: {
+              labels: Object.keys(response.data.bpi),
+              datasets: [
+                {
+                  label: `Valeur BTC en ${currency}`,
+                  data: arr,
+                  fill: true,
+                  borderColor: "rgb(75, 192, 192)",
+                  tension: 0.1,
+                },
+              ],
+            },
+          });
+        })
+        .catch((err) => console.log(err));
+}
+
+  inputFromDate = document.querySelector("#inputFromDate")
+  inputFromDate.addEventListener("change",event =>{
+    updateGraph()
   })
-  .catch((err) => console.log(err));
+  
+  inputToDate = document.querySelector("#inputToDate")
+  inputToDate.addEventListener("change",event =>{
+    updateGraph()
+  })
+  
+  // Met à jour le graphique pour chaque changement de date
+  function updateGraph(){ 
+    fromDate = inputFromDate.value
+    toDate = inputToDate.value
+    ctx.clearRect(0,0,300,150)
+    callGraph()
+  }
